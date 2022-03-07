b398783ba5bcdc5d83e5315c433e593426ab4410
Kyle Howard <kylehoward1228@yahoo.com>
2022-03-07 15:28:24 -0700

    Add javascript

--- game.js
+++ game.js
@@ -1,7 +1,30 @@
 class Game {
   constructor() {
 
+}
 
+}
+function computeScore(string1) {
+    if (string1.includes("win")) {
+        humanScore++;
+    } else if (string1.includes("lose")) {
+        computerScore++;
+    }
+    document.getElementById("humanScorePoints").innerHTML = humanScore;
+    document.getElementById("robotScorePoints").innerHTML = computerScore;
 
-  }
+    if (humanScore === 5){
+        document.getElementById("roundConclusion").innerHTML = "The winner by unanimous decision is the Human!";
+        humanScore = 0;
+        computerScore = 0;
+        document.getElementById("humanScorePoints").innerHTML = "0";
+        document.getElementById("robotScorePoints").innerHTML = "0";
+
+    } else if (computerScore === 5) {
+        document.getElementById("roundConclusion").innerHTML = "The winner by unanimous decision is the AI!";
+        humanScore = 0;
+        computerScore = 0;
+        document.getElementById("humanScorePoints").innerHTML = "0";
+        document.getElementById("robotScorePoints").innerHTML = "0";
+    }
 }
