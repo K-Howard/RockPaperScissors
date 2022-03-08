fb6445867d88ffde4c63f1c9239691ed6a5f39d6
Kyle Howard <kylehoward1228@yahoo.com>
2022-03-08 16:09:21 -0700

    Add icons and fix pictures/styling and toggle

--- game.js
+++ game.js
@@ -2,9 +2,7 @@ class Game {
   constructor() {
 
 }
-
-}
-function computeScore(string1) {
+  computeScore(string1) {
     if (string1.includes("win")) {
         humanScore++;
     } else if (string1.includes("lose")) {
@@ -14,7 +12,7 @@ function computeScore(string1) {
     document.getElementById("robotScorePoints").innerHTML = computerScore;
 
     if (humanScore === 5) {
-        document.getElementById("roundConclusion").innerHTML = "The winner by unanimous decision is the Human!";
+        document.getElementById("roundConclusion").innerHTML = "The winner by unanimous decision is YOU!";
         humanScore = 0;
         computerScore = 0;
         document.getElementById("humanScorePoints").innerHTML = "0";
@@ -27,4 +25,56 @@ function computeScore(string1) {
         document.getElementById("humanScorePoints").innerHTML = "0";
         document.getElementById("robotScorePoints").innerHTML = "0";
     }
+  }
+   playRound(playerSelection, computerSelection) {
+      if (playerSelection === 'rock' && computerSelection === 'scissors') {
+          document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You win!"
+          return ("win")
+
+      } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+          document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You win!"
+          return ("win")
+
+      } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+          document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You win!"
+          return ("win")
+
+      } else if (playerSelection === computerSelection) {
+          document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! Tie game!"
+          return ("tie")
+
+      } else if (playerSelection === 'scissors' && computerSelection === 'turkey') {
+            document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You lose!"
+            return ("win")
+
+      } else if (playerSelection === 'paper' && computerSelection === 'turkey') {
+            document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You lose!"
+            return ("win")
+
+      } else if (playerSelection === 'rock' && computerSelection === 'lizard') {
+            document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You lose!"
+            return ("win")
+
+      } else if (playerSelection === 'turkey' && computerSelection === 'lizard') {
+            document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You lose!"
+            return ("win")
+
+      } else if (playerSelection === 'turkey' && computerSelection === 'rock') {
+            document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You lose!"
+            return ("win")
+
+      } else if (playerSelection === 'lizard' && computerSelection === 'paper') {
+            document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You lose!"
+            return ("win")
+
+      } else if (playerSelection === 'lizard' && computerSelection === 'scissors') {
+            document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You lose!"
+            return ("win")
+
+      } else {
+          document.getElementById("roundConclusion").innerHTML = "Computer picked " + computerSelection + "! You lose!"
+          return ("lose")
+
+      }
+  };
 }
